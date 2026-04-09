5c3c5ce84b76ad6338cc9ab311f9758626762f68
Lingeswaran Subramaniyam <lingeswaran.s@thoughtworks.com>
2026-04-09 17:23:38 +0530

    BAH-4444 moved from entity annotation to hbm due to connection pool error

--- api/src/main/java/org/bahmni/module/eventoutbox/model/OutboxEvent.java
+++ api/src/main/java/org/bahmni/module/eventoutbox/model/OutboxEvent.java
@@ -2,31 +2,17 @@ package org.bahmni.module.eventoutbox.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
 import java.util.Date;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "event_records_queue")
 public class OutboxEvent {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String uuid;
     private String title;
     private Date timestamp;
     private String uri;
-    @Column(name = "object")
     private String content;
     private String category;
     private String tags;
-
 }
